6b00da795fba50bd3aba07368bdb1a32e08bbbdd
Sudharsan2k20 <sudharsanm204@gmail.com>
2024-07-30 20:26:26 +0530

    Database functionalities

--- contact.php
+++ contact.php
@@ -4,8 +4,7 @@
 <title>Contactus</title>
 <link rel="stylesheet" href="assets/contactstyles.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src = "https://ajax.googleapis.com/ajax/libs/jQuery/3.7.1/jQuery.min.js"></script>
-<script src="contact.js"></script>
+<!-- <script src = "https://ajax.googleapis.com/ajax/libs/jQuery/3.7.1/jQuery.min.js"></script> -->
 </head>
 <body>
 
@@ -35,19 +34,18 @@
         <div class="screen-body-item">
           <div class="app-form">
             <div class="app-form-group">
-              <input class="app-form-control" id="fname" placeholder="First Name" required>
+              <input type="text"class="app-form-control" id="fname" placeholder="First Name" required>
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" id="lname" placeholder="Last Name" required>
+              <input type="text"class="app-form-control" id="lname" placeholder="Last Name" required>
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" id="email" placeholder="Email" required>
+              <input type="email" class="app-form-control" id="email" placeholder="Email" required>
             </div>
             <div class="app-form-group">
-              <input class="app-form-control" id="number" placeholder="Contact no" required>
+              <input type="number" class="app-form-control" id="number" placeholder="Contact no" required>
             </div>
             <div class="app-form-group buttons">
-              <button class="app-form-button">CANCEL</button>
               <button class="app-form-button" id="send">SEND</button>
             </div>
           </div>
@@ -55,31 +53,35 @@
       </div>
     </div>
   </div>
+  
 </div>
 <script>
 $(document).ready(function(){
     $("#send").click(function(){
+      alert("hello");
       console.log("test");
       var first_name = $("#fname").val();
       var last_name = $("#lname").val();
       var email = $("#email").val();
       var phone = $("#number").val();
 
-      var fd = new object();
+      var fd = new FormData();
       fd.append('fname', first_name);
       fd.append('lname', last_name);
       fd.append('email', email);
       fd.append('phone', phone);
 
-      $.ajax(function(){
+      $.ajax({
         url:'function.php',
         type: 'POST',
         data: fd,
+        processData: false,
+        contentType: false,
         success: function(response){
           console.log("Inserted successfully");
         },
         error: function(xhr, status, error){
-
+          console.error("Error: " + error);
         }
       });
     })
--- function.php
+++ function.php
@@ -1,16 +1,20 @@
 <?php
 require_once('connection.php');
+
 $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$number = $_POST['phone'];
+$lname = mysqli_real_escape_string($conn, $_POST['lname']);
+$email = mysqli_real_escape_string($conn,$_POST['email']);
+$number =$_POST['phone'];
 
-$insert_query = mysqli_query("Insert into login(fname, lname, email, number") values($fname, $lname, $email, $number);
-
-if(mysqli_query($conn, $insert_query)){
+$stmt = $conn->prepare("INSERT INTO contacts(first_name, last_name, email, phone) values(?,?,?,?)");
+$stmt->bind_param("ssss", $fname, $lname, $email, $phone);
+if($stmt->execute()){
     echo "Inserted successfully";
-    
 }
+else{
+    echo "Error inserting values: " . $stmt->error;
+}
+
 
 
 
